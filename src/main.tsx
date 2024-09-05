807c869a36bcf20b43512feeb213123ea5915a86
Басайров Али Арбиевич <a.baysarov@telematika.com>
2024-09-05 19:40:33 +0400

    add pwa functionanlity

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from './theme/index.ts'
 import "./index.css"
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ChakraProvider theme={theme}>
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,40 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      outDir: "dist",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "CODD App PWA",
+        short_name: "CODD",
+        description: "Test page",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        start_url: "/",
+        display: "standalone",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    }),
     svgr({
       include: "**/**/*.svg?react"
     })],
