b563df95165fb8e5a49874b88288c626dd72ace9
Басайров Али Арбиевич <a.baysarov@telematika.com>
2024-09-05 16:21:02 +0400

    edited icons

--- src/pages/main/ui/country-list/country-list.tsx
+++ src/pages/main/ui/country-list/country-list.tsx
@@ -1,25 +1,25 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
-import { Icons } from '../../../../icons';
+import { Box, Flex, Img, Text } from '@chakra-ui/react';
 const CountryList = () => {
+    const viewBox = "0 0 56 56"
     const countries = [
         {
-            icon: <Icons.RussiaIcon width={56} height={56} />,
+            icon: <Img src={'/countries/Russia.png'} width={"56px"} height={"56px"} />,
             text: "Россия",
         },
         {
-            icon: <Icons.TurkeyIcon width={56} height={56} />,
+            icon: <Img src={'/countries/Turkey.png'} width={"56px"} height={"56px"} />,
             text: "Türk"
         },
         {
-            icon: <Icons.GeorgiaIcon width={56} height={56} />,
+            icon: <Img src={'/countries/Georgia.png'} width={"56px"} height={"56px"} />,
             text: "ქართული"
         },
         {
-            icon: <Icons.ArmeniaIcon width={56} height={56} />,
+            icon: <Img src={'/countries/Armenia.png'} width={"56px"} height={"56px"} />,
             text: "հայկ"
         },
         {
-            icon: <Icons.UKIcon width={56} height={56} />,
+            icon: <Img src={'/countries/England.png'} width={"56px"} height={"56px"} />,
             text: "England"
         }
     ];
@@ -27,11 +27,15 @@ const CountryList = () => {
         <Flex p={"27px"} direction={"column"} gap={"20px"}>
             {
                 countries.map(country => (
-                    <Box borderRadius={"5px"} transitionTimingFunction={"ease"} transitionDelay={"200"} _hover={{
-                        transform: "translateY(-5px)",
-                        transitionDuration: "350ms",
-                        boxShadow: "0px 4px 9.09px #0A488280"
-                    }}>
+                    <Box
+                        borderRadius={"5px"}
+                        padding={"3px"}
+                        transitionTimingFunction={"ease"}
+                        transitionDelay={"200"} _hover={{
+                            transform: "translateY(-5px)",
+                            transitionDuration: "350ms",
+                            boxShadow: "0px 4px 9.09px #0A488280"
+                        }}>
                         <Flex gap={31} alignItems={"center"}>
                             {country.icon}
                             <Text as={"p"}>{country.text}</Text>
