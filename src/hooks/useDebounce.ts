ba2b8667829eb8c57e9d0cf96f8f2681ba16f60a
alibajsarov <alibajsarov353@gmail.com>
2024-09-13 09:12:58 +0400

    corrected useDebounce

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,9 +1,10 @@
+
 interface HasDebounceMethod {
     debounce: (func: (...args: any[]) => void, delay: number) => (...args: any[]) => void;
 }
 
 export function useDebounce(func: (...args: any[]) => void, delay: number): (...args: any[]) => void {
-    let timeoutId: NodeJS.Timeout;
+    let timeoutId: ReturnType<typeof setTimeout>;
     return function (this: HasDebounceMethod, ...args: any[]) {
         clearTimeout(timeoutId);
         timeoutId = setTimeout(() => {
